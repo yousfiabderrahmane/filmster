42d74eeba0a2d02c8a8c6acc527c37307c346575
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-12-23 08:34:12 +0100

    main: search dark/light mode

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,9 +7,10 @@ import { useNavigate } from "react-router-dom";
 import { ReactComponent as TrendIcon } from "../assets/trending.svg";
 import { ReactComponent as FavIcon } from "../assets/favorite.svg";
 import { ReactComponent as SearchIcon } from "../assets/search.svg";
+import { ReactComponent as TV } from "../assets/tv.svg";
 
 export default function Search() {
-  const { dispatch } = useAppContext();
+  const { dispatch, mode } = useAppContext();
 
   const searchValue = useRef();
 
@@ -37,16 +38,22 @@ export default function Search() {
   return (
     <section className="search-section">
       <div className="left">
-        <button onClick={TrendRedirect}>
+        <button
+          className={`${mode === "light" && "dark-color"}`}
+          onClick={TrendRedirect}
+        >
           Trending
           <div className="trend-container">
-            <TrendIcon fill="white" />
+            <TrendIcon fill={mode === "light" ? "#121212" : "white"} />
           </div>
         </button>
-        <button onClick={favRedirect}>
+        <button
+          className={`${mode === "light" && "dark-color"}`}
+          onClick={favRedirect}
+        >
           Favorites
           <div className="trend-container">
-            <FavIcon fill="white" />
+            <FavIcon fill={mode === "light" ? "#121212" : "white"} />
           </div>
         </button>
       </div>
@@ -54,17 +61,27 @@ export default function Search() {
       <form onSubmit={handleSubmit}>
         <div className="right">
           <input
+            className={`${mode === "light" && "dark-color"}`}
             ref={searchValue}
             type="text"
             placeholder="Search For A Movie..."
           />
           <button>
             <div className="trend-container">
-              <SearchIcon fill="white" />
+              <SearchIcon fill={mode === "light" ? "#121212" : "white"} />
             </div>
           </button>
         </div>
       </form>
+
+      <div className="left">
+        <button className={`${mode === "light" && "dark-color"}`}>
+          Tv-Shows
+          <div className="trend-container">
+            <TV fill={mode === "light" ? "#121212" : "white"} />
+          </div>
+        </button>
+      </div>
     </section>
   );
 }
