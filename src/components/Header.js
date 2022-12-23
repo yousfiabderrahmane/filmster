d7e0716ea9ce2c419ef2c732560442ed27e7f4df
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-12-23 10:25:39 +0100

    mani: mode icon change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import "./Header.css";
 import { ReactComponent as MovieIcon } from "../assets/movie_FILL0_wght400_GRAD0_opsz48.svg";
-import { ReactComponent as ModeIcon } from "../assets/mode-icon.svg";
+import { ReactComponent as Light } from "../assets/light_mode_FILL0_wght400_GRAD0_opsz48 (1).svg";
+import { ReactComponent as Dark } from "../assets/dark_mode_FILL0_wght400_GRAD0_opsz48 (1).svg";
 import { useAppContext } from "../context/useAppContext";
 
 export default function Header() {
@@ -8,16 +9,29 @@ export default function Header() {
   return (
     <section className="header">
       <div className="header-svg">
-        <ModeIcon
-          onClick={() =>
-            dispatch({
-              type: "TOGGLE_MODE",
-              payload: mode === "light" ? "dark" : "light",
-            })
-          }
-          className="mode-icon"
-          fill={mode === "light" ? "#121212" : "white"}
-        />
+        {mode === "light" ? (
+          <Dark
+            onClick={() =>
+              dispatch({
+                type: "TOGGLE_MODE",
+                payload: mode === "light" ? "dark" : "light",
+              })
+            }
+            className="mode-icon"
+            fill={mode === "light" ? "#121212" : "white"}
+          />
+        ) : (
+          <Light
+            onClick={() =>
+              dispatch({
+                type: "TOGGLE_MODE",
+                payload: mode === "light" ? "dark" : "light",
+              })
+            }
+            className="mode-icon"
+            fill={mode === "light" ? "#121212" : "white"}
+          />
+        )}
       </div>
 
       <h1 className={`header-title ${mode === "light" && "dark-color"}`}>
